6628dba4571e58c9752eecdf5a326cae2d51d2af
fpem3309 <fpem3309@gmail.com>
2022-08-29 23:26:13 +0900

    email test

--- src/components/middle.vue
+++ src/components/middle.vue
@@ -8,7 +8,7 @@
     <div class="gallery" v-for="item in 10" :key="item">
       <img src="#"/>
       <div class="desc">게시물 제목</div>
-      <p>게시물 날짜</p>
+      <p>게시물 날짜!!</p>
     </div>
 
   </div>
