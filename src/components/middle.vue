1ecc2b4aec9a28361ce232487ef9a7ddd8c27792
fpem3309 <fpem3309@gmail.com>
2022-08-29 23:21:36 +0900

    a

--- src/components/middle.vue
+++ src/components/middle.vue
@@ -6,7 +6,7 @@
     <div>취향 맞춤 펀딩 프로젝트</div>
 
     <div class="gallery" v-for="item in 10" :key="item">
-      <img src="#" />
+      <img src="#"/>
       <div class="desc">게시물 제목</div>
       <p>게시물 날짜</p>
     </div>
