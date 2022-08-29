b15ea2f9d27eb80fa27b7b946f194a6fefe7f213
fpem3309 <fpem3309@gmail.com>
2022-08-29 23:10:00 +0900

    extensions 설치

--- src/components/middle.vue
+++ src/components/middle.vue
@@ -1,128 +1,24 @@
 <template>
+  <div>
     <div>
-        <div>
-            <p>배너</p>
-        </div>
+      <p>배너</p>
+    </div>
+    <div>취향 맞춤 펀딩 프로젝트</div>
 
-        <div>
-            취향 맞춤 펀딩 프로젝트
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-                <img src="img_forest.jpg" alt="Forest" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_lights.jpg">
-                <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_mountains.jpg">
-                <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-                <img src="img_forest.jpg" alt="Forest" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_lights.jpg">
-                <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_mountains.jpg">
-                <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-
-        <div class="gallery">
-            <a target="_blank" href="img_5terre.jpg">
-                <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-                <img src="img_forest.jpg" alt="Forest" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_lights.jpg">
-                <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
-        <div class="gallery">
-            <a target="_blank" href="img_mountains.jpg">
-                <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
-            </a>
-            <div class="desc">Add a description of the image here</div>
-        </div>
-
+    <div class="gallery" v-for="item in 10" :key="item">
+      <img src="#" />
+      <div class="desc">게시물 제목</div>
+      <p>게시물 날짜</p>
     </div>
 
+  </div>
 </template>
 
 <script>
-
-export default {
-
-}
+import './../css/main.css'
+export default {};
 </script>
 
 <style>
-div.gallery {
-    margin: 5px;
-    border: 1px solid #ccc;
-    float: left;
-    width: 180px;
-}
 
-div.gallery:hover {
-    border: 1px solid #777;
-}
-
-div.gallery img {
-    width: 100%;
-    height: auto;
-}
-
-div.desc {
-    padding: 15px;
-    text-align: center;
-}
 </style>
